6bb696c42e2e2276db2e579ad5fc58f3dd013bcf
octaviav <octavia7104@gmail.com>
2025-06-12 22:21:20 +0000

    First edit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
+"""
+Making changes
+"""
+
 
 def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed
+    """
     if total <= 0:
         return 0
     
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
     
-    for amnt in range(1, total + 1):
+    for i in range(1, total + 1):
         for coin in coins:
-            if coin <= amnt:
-                dp[amnt] = min(dp[amnt], dp[amnt - coin] + 1)
+            if coin <= i and dp[i - coin] != float('inf'):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
     
-    return -1 if dp[total] == float('inf') else dp[total]
-
-
-if __name__ == "__main__":
-    print(makeChange([1, 2, 25], 37))
-    print(makeChange([1256, 54, 48, 16, 102], 1453))
-    
-    print(makeChange([1, 5, 10, 25], 30))
-    print(makeChange([2], 3))
-    print(makeChange([1], 0))
-    print(makeChange([1, 3, 4], 6))
+    return dp[total] if dp[total] != float('inf') else -1
